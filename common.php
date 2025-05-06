493bb83d5015573e399b5642c43fc38ec19c1e03
gr2ws <gianasunan@gmail.com>
2025-05-06 23:14:13 +0800

    move bootstrap imports to common

--- common.php
+++ common.php
@@ -1,2 +1,14 @@
+<!-- Bootstrap CSS -->
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
+    crossorigin="anonymous" />
+<!-- Bootstrap Icons -->
+<link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+
 <!-- Put all component styles here -->
 <link rel="stylesheet" href="styles/header.css">
+<link rel="stylesheet" href="styles/footer.css">
