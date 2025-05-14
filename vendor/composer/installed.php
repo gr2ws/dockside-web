d1cc1c5706ebec01776e59fa9d66f8450563327e
gr2ws <gianasunan@gmail.com>
2025-05-15 02:04:44 +0800

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'heroku/dockside-web',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7c6fd3aa3222072bfaa498f5aa03ae2fb5c35f09',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'heroku/dockside-web' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7c6fd3aa3222072bfaa498f5aa03ae2fb5c35f09',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
